c794003cd98d882b497e9a23a10fd1f15fdcc4e1
Ashan Ekanayake <ashanekanayakeat@gmail.com>
2025-10-23 23:09:51 +0530

    change

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -10,7 +10,7 @@ public class BackendApplication {
 
 	public static void main(String[] args) {
 
-        SpringApplication.run(BackendApplication.class, args);
+        SpringApplication.run(BackendApplication.class,args);
 	}
     @Bean
     public ModelMapper modelMapper(){
